dcb774099a9654939b192c4eac611f0ffe38e4ab
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2022-08-17 14:04:46 +0800

    Initial commit

---
+++ src/main/java/com/background/system/BackgroundSystemApplication.java
@@ -0,0 +1,13 @@
+package com.background.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackgroundSystemApplication.class, args);
+    }
+
+}

b5ee0b1ffc2fdfa570e5251be9ef8dfd8a81d124
万梦辉 <1060147217@qq.COM>
2022-08-17 22:34:02 +0800

    添加注释

--- src/main/java/com/background/system/annotation/IgnoreLogin.java
+++ src/main/java/com/background/system/annotation/IgnoreLogin.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 匿名访问注解
+ *
+ * @author meng
+ */
 @Target(ElementType.METHOD) // 作用到方法上
 @Retention(RetentionPolicy.RUNTIME) // 运行时有效
 public @interface IgnoreLogin {

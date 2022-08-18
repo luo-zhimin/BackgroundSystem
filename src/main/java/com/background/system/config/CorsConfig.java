d089d5b9f2fbafedaf5d0c6d2436014641403822
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2022-08-18 14:05:19 +0800

    fix cors pattern

--- src/main/java/com/background/system/config/CorsConfig.java
+++ src/main/java/com/background/system/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
         /*是否允许请求带有验证信息*/
         corsConfiguration.setAllowCredentials(true);
         /*允许访问的客户端域名*/
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOriginPattern("*");
         /*允许服务端访问的客户端请求头*/
         corsConfiguration.addAllowedHeader("*");
         /*允许访问的方法名,GET POST等*/

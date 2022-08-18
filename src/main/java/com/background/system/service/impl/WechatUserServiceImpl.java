a5aeef6f3ab19cd07134e85709bee982de741782
menghui.wan <menghui.wan@infaith.com.cn>
2022-08-18 16:30:08 +0800

    调整返回数据结构

--- src/main/java/com/background/system/service/impl/WechatUserServiceImpl.java
+++ src/main/java/com/background/system/service/impl/WechatUserServiceImpl.java
@@ -96,7 +96,7 @@ public class WechatUserServiceImpl implements WechatUserService {
                 return result;
             }
             HashMap<String, Object> result = new HashMap<>(1);
-            result.put("returnObj", json);
+            result.put("returnObj", openId);
             return result;
         } catch (Exception e) {
             log.error("小程序获取openId出错", e);

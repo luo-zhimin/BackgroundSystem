3c4de3ef384e85e2213fd4c7efb4916d13bdb48e
menghui.wan <menghui.wan@infaith.com.cn>
2022-08-19 18:30:29 +0800

    oss工具类添加上传File类型的接口

--- src/main/java/com/background/system/util/AliUploadUtils.java
+++ src/main/java/com/background/system/util/AliUploadUtils.java
@@ -6,9 +6,7 @@ import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.model.PutObjectRequest;
 import com.aliyun.oss.model.PutObjectResult;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItem;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -24,32 +22,53 @@ import java.util.Objects;
 public class AliUploadUtils {
 
     private static final String ENDPOINT = "https://oss-cn-hangzhou.aliyuncs.com";
-    private static final String RESULTURL = "https://asugar.oss-cn-hangzhou.aliyuncs.com";
+    private static final String RESULT_URL = "https://asugar.oss-cn-hangzhou.aliyuncs.com";
     private static final String ACCESS_KEY_ID = "LTAI5tJdGdn4guLATNEwNJft";
     private static final String ACCESS_KEY_SECRET = "C66R67VRkLqihwJnSRD0knnhzkPhja";
     private static final String BUCKET_NAME = "asugar";
 
     /**
-     *
-     * @param file  文件流
+     * @param file   文件流
      * @param father 上级目录（上传哪个文件夹）
      */
-    public static String upload(MultipartFile file, String father) {
+    public static String uploadImage(MultipartFile file, String father) {
 
         OSS ossClient = new OSSClientBuilder().build(ENDPOINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
-        String type = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf("."));
+        String type =
+            Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf("."));
         String title = UUID.randomUUID().toString();
         String path = father + "/" + title + type;
         try {
-            PutObjectRequest request = new PutObjectRequest(BUCKET_NAME, path, new ByteArrayInputStream(file.getBytes()));
+            PutObjectRequest request =
+                new PutObjectRequest(BUCKET_NAME, path, new ByteArrayInputStream(file.getBytes()));
             PutObjectResult putObjectResult = ossClient.putObject(request);
             PushPlusUtils.push(putObjectResult);
             log.info("[上传信息] - " + putObjectResult);
         } catch (IOException e) {
             return "";
-        }finally {
+        } finally {
             ossClient.shutdown();
         }
-        return RESULTURL + "/" + father + "/" + title + type;
+        return RESULT_URL + "/" + father + "/" + title + type;
+    }
+
+    /**
+     * 上传文件
+     *
+     * @param file
+     * @param path
+     * @return
+     */
+    public static String uploadPdf(File file, String path) {
+        path = path + "_" + System.currentTimeMillis() + "_" + UUID.randomUUID();
+        OSS ossClient = new OSSClientBuilder().build(ENDPOINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+        try {
+
+            PushPlusUtils.push(ossClient.putObject(BUCKET_NAME, path, file));
+        } finally {
+            ossClient.shutdown();
+        }
+
+        return RESULT_URL + "/" + path;
     }
 }

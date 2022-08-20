485ff0976e419844e2dbe4bb97b9cabb8c8bcfd2
方糖 <xiaoheikeji@vip.qq.com>
2022-08-20 15:56:32 +0800

    [add] 订单管理

---
+++ src/main/java/com/background/system/controller/OrderController.java
@@ -0,0 +1,113 @@
+package com.background.system.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.background.system.entity.*;
+import com.background.system.entity.vo.OrderVo;
+import com.background.system.mapper.*;
+import com.background.system.util.AliUploadUtils;
+import com.background.system.util.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+/**
+ * @Description:
+ * @Author: 方糖
+ * @Date: 2022/8/20 3:15 PM
+ */
+@Api("订单管理")
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Resource
+    private OrderMapper orderMapper;
+
+    @Resource
+    private SizeMapper sizeMapper;
+
+    @Resource
+    private CaizhiMapper caizhiMapper;
+
+    @Resource
+    private CouponMapper couponMapper;
+
+    @Resource
+    private PictureMapper pictureMapper;
+
+    /**
+     * 返回订单号
+     * @param orderVo
+     * @return
+     */
+    @PostMapping("create")
+    @ApiOperation("创建订单")
+    public Result<?> createOrder(@RequestBody OrderVo orderVo) {
+        Order order = new Order();
+        BeanUtil.copyProperties(orderVo, order);
+        // 计算价格
+        Size size = sizeMapper.selectById(order.getSizeId());
+        Caizhi caizhi = caizhiMapper.selectById(order.getCaizhiId());
+        BigDecimal uPrice = size.getUPrice();
+        BigDecimal price = caizhi.getPrice();
+        order.setTotal(uPrice.add(price));
+        orderMapper.insert(order);
+        return Result.success(order.getId());
+    }
+
+    /**
+     * 返回优惠后的金额
+     * @param couponId
+     * @param orderId
+     * @return
+     */
+    @GetMapping("coupon")
+    @ApiOperation("使用优惠券")
+    public Result<?> coupon(String couponId, String orderId) {
+        Coupon coupon = couponMapper.selectById(couponId);
+        BigDecimal price = coupon.getPrice();
+        Order order = orderMapper.selectById(orderId);
+        BigDecimal total = order.getTotal();
+        BigDecimal newTotal = total.subtract(price);
+        if (newTotal.compareTo(BigDecimal.ZERO) <= 0) {
+            newTotal = BigDecimal.ZERO;
+        }
+        order.setTotal(newTotal);
+        orderMapper.updateById(order);
+        return Result.success(newTotal);
+    }
+
+    /**
+     * 订单界面修改地址（选择地址列表已有里的地址，新建修改调用地址接口去）
+     * @param orderId
+     * @param addressId
+     * @return
+     */
+    @GetMapping("changeAddress")
+    @ApiOperation("修改地址-选择列表已有的")
+    public Result<?> changeAddress(String orderId, Long addressId) {
+        Order order = orderMapper.selectById(orderId);
+        order.setAddressId(addressId);
+        return Result.success(orderMapper.updateById(order));
+    }
+
+    /**
+     * 上传图片返回图片ID
+     * @param file
+     * @return
+     */
+    @PostMapping("getPictute")
+    @ApiOperation("上传图片")
+    public Result<?> getPictute(@RequestBody MultipartFile file) {
+        String aDefault = AliUploadUtils.uploadImage(file, "default");
+        Picture picture = new Picture();
+        picture.setUrl(aDefault);
+        int insert = pictureMapper.insert(picture);
+        return Result.success(picture.getId());
+    }
+
+}

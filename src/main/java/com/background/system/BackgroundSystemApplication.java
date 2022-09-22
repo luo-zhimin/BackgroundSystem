10aef05fc26b2362b2f471708c7fc6086204686f
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2022-09-22 11:49:04 +0800

    fix-scheduling zip

--- src/main/java/com/background/system/BackgroundSystemApplication.java
+++ src/main/java/com/background/system/BackgroundSystemApplication.java
@@ -4,10 +4,12 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.background.system.mapper")
 @EnableEncryptableProperties
+@EnableScheduling
 public class BackgroundSystemApplication {
 
     public static void main(String[] args) {
